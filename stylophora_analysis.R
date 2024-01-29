78b7d8512f175595b151879032d897d5b9af1cd1
PaulineGnsr <pauline.guenser@gmail.com>
2024-01-29 16:38:38 +0100

    Format for publication

--- stylophora_analysis.R
+++ stylophora_analysis.R
@@ -12,20 +12,64 @@ library(ggrepel)
 library(ggpubr)
 library(viridis)
 
-setwd("C:/Users/33677/Documents/NEW_WORK/Congrès/ECE11 2023/data")
+setwd("C:/Users/33677/Documents/NEW_WORK/Postdoc LYON/Stylophora Colonialism/Stylophora_Colonialism")
 stylo <- read.table("stylophoran material.txt", sep="\t", row.names = 1, h=T, stringsAsFactors = TRUE)
 
 
 ## Google search ####
 google.scholar <- read.table("Google_search.txt", sep="\t", row.names = 1, h=T, stringsAsFactors = TRUE)
-jpeg(filename = "Google_search.jpeg", width=12, height=6, units="cm", res=300, pointsize = 8)
-par()
+# jpeg(filename = "Fig1_Google_search.jpeg", width=12, height=6, units="cm", res=300, pointsize = 8)
 barplot(t(google.scholar), beside = TRUE, col=viridis(3),
         ylim=c(0,700), names.arg = rownames(google.scholar),
         ylab="Number of publications", xlab="Time",
         main="Evolution of the use of terms (Google Scholar)",
         legend.text = TRUE, args.legend = list(bty="n", x="topleft", ncol=1))
-dev.off()
+# dev.off()
+
+## Time-dependant plot
+# svg(filename = "Fig2A_barplot local.svg", width=7, height=1.75, pointsize = 8)
+hist(stylo$year, right=FALSE,
+     xlab="", ylab="Number of described holotypes", main="",
+     breaks=seq(1850,2025,25), xlim=range(1850,2050), ylim=range(0,60), labels = TRUE)
+# dev.off()
+
+
+## Local co-authoring ####
+stylo <- stylo%>%mutate(year_interval = case_when((year<1925 ~ "before 1925"), 
+                                                  (year>=1925&year<1975 ~ "1925-1974"), 
+                                                  (year>=1975&year<2000 ~ "1975-1999"),
+                                                  (year>=2000&year<2025 ~ "since 2000")))
+stylo.national.data <- 
+  stylo %>% dplyr::select(national.authors, year_interval, origin) %>% unique() %>% split(., .$year_interval)
+plot.national <- function(data){
+  plot.oritime <- 
+    ggplot() + country_shapes + maptheme + mapcoords+
+    geom_polygon(
+      data = ggplot2::map_data(map="world", region = data$origin),
+      aes(x = long, y = lat, group = group),
+      fill = "goldenrod", color = "black", size = 0.1)+
+    labs(title = data$year_interval)+
+    theme(plot.title = element_text(size=6))
+  
+  national.data <- as.data.frame(table(data$national.authors))
+  pie.nat <-
+    ggplot(data=national.data, 
+           aes(x="", y=Freq, fill=Var1)) +
+    geom_bar(stat="identity", width=1) +
+    geom_text(aes(label = Var1), size=1,
+              position = position_stack(vjust = 0.5)) +
+    coord_polar("y", start=0) +
+    theme_nothing()
+  
+  plot.oritime + patchwork::inset_element(pie.nat, 
+                                          left = 0.03, bottom = -0.1, right = 0.23, top = .7,
+                                          align_to = "full")
+}
+
+plot.natoritime <- lapply(stylo.national.data, plot.national)
+plot.natoritime.list <- ggpubr::ggarrange(plotlist = plot.natoritime, ncol = 1, nrow = 4)
+# ggsave(filename = "Fig2B_plot natoritime.svg", plot = plot.natoritime.list, 
+#        device = "svg", height = 13, units = "cm")
 
 
 ## Mapping ####
@@ -109,64 +153,5 @@ ggplot(nodes) + country_shapes +
                   size = 2, color = "grey30", fontface = "bold") +
   mapcoords + maptheme
 
-ggsave(file="holotypes.jpeg", plot=ggholotypes, width=16, height=9, units="cm")
+ggsave(file="Fig3_holotypes.jpeg", plot=ggholotypes, width=16, height=9, units="cm")
 
-
-  ## Time-dependant plot
-svg(filename = "barplot local.svg", width=7, height=1.75, pointsize = 8)
-hist(stylo$year, right=FALSE,
-     xlab="", ylab="Number of described holotypes", main="",
-     breaks=seq(1850,2025,25), xlim=range(1850,2050), ylim=range(0,60), labels = TRUE)
-dev.off()
-
-stylo <- stylo%>%mutate(year_interval = case_when((year<1925 ~ "before 1925"), 
-                                                  (year>=1925&year<1975 ~ "1925-1974"), 
-                                                  (year>=1975&year<2000 ~ "1975-1999"),
-                                                  (year>=2000&year<2025 ~ "since 2000")))
-## Local co-authoring ####
-stylo.national.data <- 
-  stylo %>% dplyr::select(national.authors, year_interval, origin) %>% unique() %>% split(., .$year_interval)
-plot.national <- function(data){
-  plot.oritime <- 
-    ggplot() + country_shapes + maptheme + mapcoords+
-    geom_polygon(
-      data = ggplot2::map_data(map="world", region = data$origin),
-      aes(x = long, y = lat, group = group),
-      fill = "goldenrod", color = "black", size = 0.1)+
-    labs(title = data$year_interval)+
-    theme(plot.title = element_text(size=6))
-  
-  national.data <- as.data.frame(table(data$national.authors))
-  pie.nat <-
-    ggplot(data=national.data, 
-           aes(x="", y=Freq, fill=Var1)) +
-    geom_bar(stat="identity", width=1) +
-    geom_text(aes(label = Var1), size=1,
-              position = position_stack(vjust = 0.5)) +
-    coord_polar("y", start=0) +
-    theme_nothing()
-  
-  plot.oritime + patchwork::inset_element(pie.nat, 
-                                          left = 0.03, bottom = -0.1, right = 0.23, top = .7,
-                                          align_to = "full")
-}
-# plot.national(stylo.national.data$`1925-1974`)
-plot.natoritime <- lapply(stylo.national.data, plot.national)
-plot.natoritime.list <- ggpubr::ggarrange(plotlist = plot.natoritime, ncol = 1, nrow = 4)
-ggsave(filename = "plot natoritime.svg", plot = plot.natoritime.list, 
-       device = "svg", height = 13, units = "cm")
-
-national.data.1 <- as.data.frame(table(national.data$`before 1925`$national.authors))
-# Basic piechart
-pie.1 <- 
-  ggplot(data=national.data.1, 
-       aes(x="", y=Freq, fill=Var1)) +
-  geom_bar(stat="identity", width=1) +
-  geom_text(aes(label = Var1),
-            position = position_stack(vjust = 0.5)) +
-  coord_polar("y", start=0) +
-  theme_nothing()
-pie.1
-
-plot.1850 + patchwork::inset_element(pie.1, left = 0, bottom = -0.1, right = 0.25, top = .7,
-                                     align_to = "full")
